113dd8762037990fb98d8925daeb0d91ddfa12d0
sebv <sebv@sv.home>
2020-03-10 16:50:25 +0100

    step two : save data into LS

--- script.js
+++ script.js
@@ -7,10 +7,30 @@ const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 let ticketPrice = +movieSelect.value; // convert to a number with sign '+'
 
+// Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
 // Update total and count
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
   //   console.log(selectedSeats);
+
+  // To save selected seats, we need to =>
+
+  // Copy selected seats into array with spread operator
+  // Map trought array
+  // Return a new array indexes
+  const seatsIndex = [...selectedSeats].map(seat => {
+    return [...seats].indexOf(seat);
+  });
+  // console.log(seatsIndex);
+
+  // Save into LS
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex)); // setItem need a key and value which transform it JSON format with JSON.stringify
+
   const selectedSeatsCount = selectedSeats.length;
   //   console.log(selectedSeatsCount);
   count.innerText = selectedSeatsCount;
@@ -20,6 +40,10 @@ function updateSelectedCount() {
 // Update price select movie
 movieSelect.addEventListener('change', e => {
   ticketPrice = +e.target.value;
+
+  // console.log(e.target.selectedIndex, e.target.value); // select movie
+  setMovieData(e.target.selectedIndex, e.target.value); // select movie
+
   updateSelectedCount();
 });
 
